b6bdfddbd65051699bdaa99071ecfcf540f2fa6e
kookiekenobi <kookiekenobi@gmail.com>
2025-01-29 05:02:18 +0800

    game: added randomized debug dialogue

--- SSLR/Assets/Scripts/NPCBehaviour.cs
+++ SSLR/Assets/Scripts/NPCBehaviour.cs
@@ -8,28 +8,47 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class NPCBehaviour : MonoBehaviour
 {
     private NPCMovement npcMoveScript;
     [SerializeField] private GameObject paperObject;
-    [SerializeField] private GameObject navigationTxt;
+    [SerializeField] private GameObject navigationTxtBubble; 
+    [SerializeField] TextMeshProUGUI navigationTxt;
+
+    private string[] possibleSentences =
+    {
+        "I want to go left!",
+        "I want to go right!"
+    };
 
     void Start()
     {
         npcMoveScript = GetComponent<NPCMovement>();
         
         paperObject.SetActive(false);
-        navigationTxt.SetActive(false);
+        navigationTxtBubble.SetActive(false);
+        
+        if (navigationTxt != null)
+        {
+            navigationTxt.text = GetRandomSentence();
+        }
     }
     
     void Update()
     {
         if (npcMoveScript.inFrontOfPlayer == true)
         {
-            paperObject.SetActive(true);
-            navigationTxt.SetActive(true);
+            //paperObject.SetActive(true);
+            navigationTxtBubble.SetActive(true);
         }
+    }
+
+    string GetRandomSentence()
+    {
+        int n = Random.Range(0, possibleSentences.Length);
+        return possibleSentences[n];
     }
 }
