7841ceaedd74f90a982ebbc1d424f8d3f943b31f
sippy-thinks <s10257006@connect.np.edu.sg>
2025-02-03 20:22:01 +0800

    game: stamp done :3

--- SSLR/Assets/Scripts/StampInteractor.cs
+++ SSLR/Assets/Scripts/StampInteractor.cs
@@ -1,18 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class StampInteractor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   private void OnCollisionEnter(Collision other)
+   {
+       if (StampSocketInteractor.isHeld && other.gameObject.CompareTag("Paper"))
+       {
+           Debug.Log("paper is stamped");
+       }
+   }
 }
---
+++ SSLR/Assets/Scripts/StampSocketInteractor.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+public class StampSocketInteractor : MonoBehaviour
+{
+    XRSocketInteractor stampSocket;
+    public static bool isHeld = false;
+
+
+    private void Awake()
+    {
+        stampSocket = GetComponent<XRSocketInteractor>();
+    }
+
+    void OnEnable()
+    {
+        stampSocket.selectEntered.AddListener(OnStampTable);
+        stampSocket.selectExited.AddListener(OnStampHeld);
+        
+        
+    }
+
+    private void OnDisable()
+    {
+        stampSocket.selectEntered.RemoveListener(OnStampTable);
+        stampSocket.selectExited.RemoveListener(OnStampHeld);
+    }
+
+
+    private void OnStampHeld(SelectExitEventArgs args)
+    {
+        isHeld = true;
+        Debug.Log("Stamp is held");
+    }
+
+    private void OnStampTable(SelectEnterEventArgs args)
+    {
+        isHeld = false;
+        Debug.Log("Stamp is on table");
+    }
+    
+}
