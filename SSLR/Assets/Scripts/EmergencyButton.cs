fe47024f0b620d98535d5a07a5e63bc101c29a2c
ryan <ryan@scorchthinks.dev>
2025-02-10 18:45:29 +0800

    game: interaction complete

---
+++ SSLR/Assets/Scripts/EmergencyButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmergencyButton : MonoBehaviour
+{
+    public void test()
+    {
+        Debug.Log("Emergency Button Pressed");
+    }
+}
