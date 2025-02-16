ee21413f73fa59fb1229ff2cda161f8c5698543a
kookiekenobi <kookiekenobi@gmail.com>
2025-02-17 05:46:23 +0800

    game: game scene desk area

---
+++ SSLR/Assets/Scripts/UIFacePlayer.cs
@@ -0,0 +1,31 @@
+/*
+ * Author: Livinia Poo
+ * Date: 17/2/25
+ * Description: 
+ * For UI Labels to Face Player
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFacePlayer : MonoBehaviour
+{
+    public Transform playerCam;
+
+    void Start()
+    {
+        if (Camera.main != null)
+        {
+            playerCam = Camera.main.transform;
+        }
+    }
+
+    void Update()
+    {
+        if (playerCam != null)
+        {
+            transform.LookAt(transform.position + playerCam.forward);
+        }
+    }
+}
