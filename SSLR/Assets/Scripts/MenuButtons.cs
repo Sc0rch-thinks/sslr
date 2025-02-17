0094c0d7e46bd0faae828b17221f448ce938f289
ryan <ryan@scorchthinks.dev>
2025-02-17 16:35:26 +0800

    game: fixing auth scene change

--- SSLR/Assets/Scripts/MenuButtons.cs
+++ SSLR/Assets/Scripts/MenuButtons.cs
@@ -5,21 +5,23 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-    
+
 public class MenuButtons : MonoBehaviour
 {
     /// <summary>
     /// Assigning Variables
     /// </summary>
     public TMP_InputField loginEmailInput;
+
     public TMP_InputField loginPasswordInput;
-    
+
     public TextMeshProUGUI usernameText;
     public TextMeshProUGUI daysPlayedText;
     public TextMeshProUGUI peopleHelpedText;
     public TextMeshProUGUI accuracyText;
-    
+
     public Image profilePicture;
+
     /// <summary>
     /// Calling backend to log player in
     /// </summary>
@@ -53,15 +55,15 @@ public class MenuButtons : MonoBehaviour
         if (user != null)
         {
             var totalPeopleHelped = user.customersHelpedCorrectly + user.customersHelpedWrongly;
-            var accuracy = totalPeopleHelped > 0? ((float)user.customersHelpedCorrectly/totalPeopleHelped)*100:0;
-            
+            var accuracy = totalPeopleHelped > 0 ? ((float)user.customersHelpedCorrectly / totalPeopleHelped) * 100 : 0;
+
             Debug.Log($"Updating UI: {user.displayName}, Days Played: {user.daysPlayed}, Accuracy: {accuracy}%");
-            
+
             usernameText.text = user.displayName;
             daysPlayedText.text = user.daysPlayed.ToString();
             peopleHelpedText.text = totalPeopleHelped.ToString();
             accuracyText.text = $"{accuracy:F2}%";
-            
+
             GetProfilePicture();
         }
         else
@@ -69,22 +71,23 @@ public class MenuButtons : MonoBehaviour
             Debug.LogError("User data is null. UI not updated");
         }
     }
+
     public void Logout()
     {
         Backend.instance.SignOut();
     }
-    
+
     public void GetProfilePicture()
     {
         // profilePicture.sprite = Backend.instance.GetProfilePicture("https://fchobpauqasfebohuuam.supabase.co/storage/v1/object/public/Avatar//1739708594780-6156448458035283230_120.jpg");
-        
-        if(Backend.instance.User != null && !string.IsNullOrEmpty(Backend.instance.User.profilePictureUrl))
-        Backend.instance.GetProfile(Backend.instance.User.profilePictureUrl, profilePicture);
+
+        if (Backend.instance.User != null && !string.IsNullOrEmpty(Backend.instance.User.profilePictureUrl))
+            Backend.instance.GetProfile(Backend.instance.User.profilePictureUrl, profilePicture);
     }
 
     public void LoadScene(string scene)
     {
-        LoadScene(scene);
+        SceneManager.LoadScene(scene);
     }
 
     public void QuitGame()
